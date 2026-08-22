f4e2ab6e65ae7c001487a1cec09c74fb6c35d6ac
DHARANIVENDHANV28 <dharanivendhanv01@gmail.com>
2026-08-22 10:22:07 +0530

    Add: binary-tree-vertical-order-traversal - submission-4

---
+++ Data Structures & Algorithms/binary-tree-vertical-order-traversal/submission-4.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        HashMap = {} #{(row,col):node.val}
+
+        if not root:
+            return []
+        c_min,c_max = float('+inf'), float('-inf')
+        r_max = 0 
+        def dfs(node,row,col):
+            nonlocal c_min,c_max,r_max
+            if not node:
+                return None
+            if (row,col) not in HashMap:
+                HashMap[(row,col)] = []
+            HashMap[(row,col)] += [node.val]
+            
+            c_min = min(c_min,col)
+            c_max = max(c_max,col)
+            r_max = max(r_max,row)
+            
+            dfs(node.left,row+1,col-1)
+            dfs(node.right,row+1,col+1)
+            return None
+        dfs(root,0,0)
+
+        # c_min,c_max = float('+inf'), float('-inf')
+        # r_min,r_max = 0,0
+        # for k,v in HashMap.items():
+        #     c_min = min(c_min,k[1])
+        #     c_max = max(c_max,k[1])
+        #     r_max = max(r_max,k[0])
+
+        res = []
+
+        for c in range(c_min,c_max+1):
+            col = []
+            for r in range(0,r_max+1):
+                if (r,c) in HashMap:
+                    col.extend(HashMap[(r,c)])
+            res.append(col)
+
+        return res
+        
